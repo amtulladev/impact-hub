650d03a4c08f3f6c90e27447731657cdf13ac8b6
Amtulla Baranwala <amtullabaranwala@gmail.com>
2023-12-07 17:32:06 +0530

    feat: footer completed

---
+++ src/components/Footer.tsx
@@ -0,0 +1,25 @@
+export default function Footer() {
+  return (
+    <footer className=" bg-secondary py-6 text-white md:py-8">
+      <section className="px-5 md:mx-auto  md:flex md:max-w-[1200px] md:justify-between md:px-0">
+        <p className="text-center md:text-left">
+          © 2022 ImpactHub All Rights Reserved By ImpactHub
+        </p>
+        <section className="flex justify-center pt-5 md:pt-0">
+          <a
+            href="#"
+            className="mr-8 hover:font-medium hover:text-black focus:font-medium focus:text-black"
+          >
+            FAQ
+          </a>
+          <a
+            href="#"
+            className="hover:font-medium hover:text-black focus:font-medium focus:text-black"
+          >
+            Support
+          </a>
+        </section>
+      </section>
+    </footer>
+  );
+}
