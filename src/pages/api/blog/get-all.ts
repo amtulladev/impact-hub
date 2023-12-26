007985b59f223c47ba672bce2c618f3ef14091a7
Amtulla Baranwala <amtullabaranwala@gmail.com>
2023-12-26 18:44:37 +0530

    fix: change api from post to get method

--- src/pages/api/blog/get-all.ts
+++ src/pages/api/blog/get-all.ts
@@ -5,11 +5,11 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  if (req.method !== "POST") {
+  if (req.method !== "GET") {
     return res.status(405).end();
   }
 
-  const { userId } = req.body;
+  const { userId } = req.query;
 
   try {
     const { db } = await connectToDatabase();
